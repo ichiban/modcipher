92e2c3b6e7e070970bdbc2837d96675272ad8656
Yutaka Ichibangase <yichiban@gmail.com>
2024-09-01 14:13:38 +0900

    remove unused req

--- cmd/modcipher/main.go
+++ cmd/modcipher/main.go
@@ -41,27 +41,6 @@ func main() {
 	}
 
 	for _, arg := range flag.Args() {
-		req, err := http.NewRequest(http.MethodGet, arg, nil)
-		if err != nil {
-			slog.Error("NewRequest failed", "err", err)
-			continue
-		}
-
-		req.Header.Set("Accept", "application/json")
-		req.Header.Set("Accept-Encoding", "gzip, deflate, br, zstd")
-		req.Header.Set("Accept-Language", "en-US,en;q=0.9,ja;q=0.8")
-		req.Header.Set("Host", "httpbin.org")
-		req.Header.Set("Priority", "u=1, i")
-		req.Header.Set("Referer", "https,//httpbin.org/")
-		req.Header.Set("Sec-Ch-Ua", "\"Not)A;Brand\";v=\"99\", \"Google Chrome\";v=\"127\", \"Chromium\";v=\"127\"")
-		req.Header.Set("Sec-Ch-Ua-Mobile", "?0")
-		req.Header.Set("Sec-Ch-Ua-Platform", "\"macOS\"")
-		req.Header.Set("Sec-Fetch-Dest", "empty")
-		req.Header.Set("Sec-Fetch-Mode", "cors")
-		req.Header.Set("Sec-Fetch-Site", "same-origin")
-		req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36")
-		req.Header.Set("X-Amzn-Trace-Id", "Root=1-66d2fdf0-41deabbe241611ad5e8c4f6a")
-
 		resp, err := http.Get(arg)
 		if err != nil {
 			slog.Error("GET failed", "err", err)
